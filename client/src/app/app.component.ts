fc0918838c7e2bc12e41c700addb0ddb8050c1d5
giuratofabrizio <giurato.fabrizio@iisgalvanimi.edu.it>
2024-05-17 11:00:43 +0000

    punto 3

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,13 +1,23 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'client';
+  data = {};
+  constructor(private http: HttpClient)
+  {
+    this.http.get("https://5000-giuratofabr-progettodoc-v1capaj17qy.ws-eu111.gitpod.io/simple_json")
+    .subscribe(
+      (data) => 
+      {
+        this.data = data;
+        console.log(data);
+      }
+    )
+  }
 }
